e7f587a42f51cef2dd72db6416667d17822f99a5
hutchgrant <h.g.utchinson@gmail.com>
2014-09-03 14:27:21 -0400

    removing debug output

--- uc_cc/uc_cc.js
+++ uc_cc/uc_cc.js
@@ -17,7 +17,6 @@ jQuery(document).ready(function ($) {
         timeleft = timeleft -1;
         if(timeleft <= 0)
         {
-            console.log('time up');
             clearInterval(countdown);
             countdown = 0;
             clearInterval(checker);
@@ -30,19 +29,15 @@ jQuery(document).ready(function ($) {
         var seconds_string = "0" + seconds;
         seconds_string = seconds_string.substr(seconds_string.length - 2);
         if(  document.getElementById('cc_progress_timer') != null) {
-		document.getElementById('cc_progress_timer').innerHTML = minutes + ":" + seconds_string;
-    
-	}else{
-		timeleft = 0;	
-	}
+			document.getElementById('cc_progress_timer').innerHTML = minutes + ":" + seconds_string;
+    	}else{
+			timeleft = 0;	
+		}
     }
 
   function submit_uc_cc_form() {
 
-    console.log('submitted');
-
     if(typeof timeleft == 'undefined') {
-      console.log('no time');
       timeleft = (Drupal.settings.uc_cc.time_limit);
 	if(selCoin == "cc_coin_btc"){
 		timeleft = timeleft * 2;
@@ -50,22 +45,14 @@ jQuery(document).ready(function ($) {
 	}
        countdown = setInterval(timer, iVal); //start timer
     } else {
-        console.log(timeleft);
-        console.log('complete');
         $('form#uc-cc-form').submit();
     }
   }
-
-  $('#edit-submit').mousedown(function() {
-    console.log('in');
-  });
-
     $("body").click(function(e) {
       $("#cc_coin_reselect").mousedown(function() {
-        console.log('reselect');
         clearInterval(checker); //reset timer
         checker = 0;
-        document.getElementById('cc_payment_information').innerHTML = '<p>Please select another currency to continue checkout</p>'; //TODO should be in php
+        document.getElementById('cc_payment_information').innerHTML = '<p>Please select another currency to continue checkout</p>'; 
 
         $("#cc_coin_select_wrapper").fadeTo("slow", 1.00, function(){ //fade and toggle class
              $(this).slideDown("slow");
@@ -80,13 +67,7 @@ jQuery(document).ready(function ($) {
       });
 
       if($(e.target).parent().is('#cc_coin_select_wrapper')) {
-        console.log(e.target.id);
-        console.log('target');
 	selCoin = e.target.id;
-           /*   if( selCoin == "cc_coin_btc"){
-                timeleft= timeleft * 10;
-                iVal = iVal * 10;
-        } */
 
         $('[name="uc_cc_selected_coin"]').val(e.target.id);
         $('#cc_payment_processing_wrapper').show();
